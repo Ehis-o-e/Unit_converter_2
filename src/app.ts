405b5018902c46c4ad446e13859a6f06711f596d
Ehis-o-e <his.ehinomhen@gmail.com>
2025-10-25 09:50:13 +0100

    adding build and start file

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('.')); 
+app.use(express.static('.'));  
 
 const PORT = 3000;
 
